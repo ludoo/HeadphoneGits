da00b6f6f0fcc6c24a24141b69aa2a67429362c8
Ludo <ludomagno@google.com>
2024-04-21 08:13:46 +0200

    image sync

--- tools/image-sync.py
+++ tools/image-sync.py
@@ -39,7 +39,7 @@ def _get_image_from_cdn(abspath, url):
   abspath = '.'.join([abspath, content_type.split('/')[1]])
   logging.info(f'-- storing {abspath}')
   try:
-    os.makedirs(os.dirname(abspath), exist_ok=True)
+    os.makedirs(os.path.dirname(abspath), exist_ok=True)
   except Exception as e:
     raise Error(f'Cannot create folder. {e.args[0]}')
   try:
